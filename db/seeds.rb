07aac2856c14f31f218f0d18882a93e73243beb1
anapaulalg <ana_paula_lg@hotmail.com>
2017-10-16 16:17:21 +0200

    Add, edit, update and delete biography

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 Biography.destroy_all
 
-bio1 = Biography.create!( { title: "Gabriela", content: "Ik wilde graag iets doen met mijn creativiteit en passie voor smaak.", picture: "https://www.instagram.com/p/BZVn1lNnAbv/?taken-by=quericowoerden" } )
-bio2 = Biography.create!( { title: "", content: "I would like to do something with my creativity and passion for taste.", picture: "" } )
+bio1 = Biography.create!( { title: "Gabriela - dutch", content: "Ik wilde graag iets doen met mijn creativiteit en passie voor smaak.", picture: "http://woerden.hu.nl/wp-content/uploads/sites/12/resized/2017-09-Que-Rico--840x560-cropped.jpg?img2x" } )
+bio2 = Biography.create!( { title: "Gabriela - english", content: "I would like to do something with my creativity and passion for taste.", picture: "http://woerden.hu.nl/wp-content/uploads/sites/12/resized/2017-09-Que-Rico--420x280-cropped.jpg" } )
