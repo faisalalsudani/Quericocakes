1088e973c96d071d9ec0833010fa4fd0a39183ca
Matthijs <matthijs_mandjes@hotmail.com>
2017-10-16 15:25:26 +0200

    update post added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+  before_action :set_post, only: [:edit, :update]
 
   def new
     @post = Post.new
@@ -16,14 +16,22 @@ class PostsController < ApplicationController
   end
 
   def edit
-
+    # id in path to edit page
   end
 
   def update
-
+    if(@post.update_attributes(post_params))
+      # redirect to somewhere
+    else
+      # render form again
+    end
   end
 
 private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :content, :picture)
