b835886b4b1cfb85d15526dd38294c42328e18cf
faisalalsudani <f4desiign@gmail.com>
2017-10-18 15:06:08 +0200

    not to merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources 'contacts', only: [:new, :create], path_names: { new: '' }
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  get 'contacts/create'
+
   devise_for :users, controllers: { registrations: "users" }
   root 'homepages#index'
   resources :biographies
