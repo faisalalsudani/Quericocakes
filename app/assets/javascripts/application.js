079f08a233fd770aa2d197b591bfc419e2c9be30
faisalalsudani <f4desiign@gmail.com>
2017-10-19 14:42:32 +0200

    errors msg

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,31 +5,6 @@
 //= require jquery_ujs
 //= require_tree .
 $(document).ready(function() {
-
-  $(":checkbox").on("change", function() {
-    var checkboxValues = {};
-    $(":checkbox").each(function() {
-      checkboxValues[this.id] = this.checked;
-    });
-    $.cookie('checkboxValues', checkboxValues, {
-      expires: 7,
-      path: '/'
-    })
-  });
-
-  function repopulateCheckboxes() {
-    var checkboxValues = $.cookie('checkboxValues');
-    if (checkboxValues) {
-      Object.keys(checkboxValues).forEach(function(element) {
-        var checked = checkboxValues[element];
-        $("#" + element).prop('checked', checked);
-      });
-    }
-  }
-
-  $.cookie.json = true;
-  repopulateCheckboxes();
-
   var output = document.getElementById("display_here");
   $("#size").click(function() {
 
