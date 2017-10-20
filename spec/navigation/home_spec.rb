052e725667ca590f8fb384d75927b5a67fb42af2
anapaulalg <ana_paula_lg@hotmail.com>
2017-10-20 14:00:24 +0200

    Add models tests

---
+++ spec/navigation/home_spec.rb
@@ -0,0 +1,10 @@
+# require "rails_helper"
+#
+# describe "Home page to build your cake" do
+#   it "go to build your cake page" do
+#     visit root_path
+#     click_on "MAKE YOUR OWN CAKE"
+#
+#     expect(page).to have_content "Cake flavor"
+#   end
+# end
