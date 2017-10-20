3af53af418403089c9a66402041daf47b6ea7ac4
Matthijs <matthijs_mandjes@hotmail.com>
2017-10-20 16:31:00 +0200

    authentication for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def new
     @post = Post.new
